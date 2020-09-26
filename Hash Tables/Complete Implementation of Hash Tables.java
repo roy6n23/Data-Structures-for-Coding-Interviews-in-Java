304499f2d4540df346c3042aa3c980e6354a20ff
Edu Roy <1610132@mail.nankai.edu.cn>
2020-09-26 16:29:05 +0800

    all chanllenge finished

--- Hash Tables/Complete Implementation of Hash Tables.java
+++ Hash Tables/Complete Implementation of Hash Tables.java
@@ -135,3 +135,48 @@ class HashTable
 		}
 	}
 }
+
+
+
+//-----------------------main.java
+class HashTableDemo {
+	public static void main(String[] args)
+	{
+    HashTable table = new HashTable(); //Create a HashTable
+    //Before Insertion
+ 		System.out.println("Table Size = " + table.size());  
+		table.insert("This",1 ); //Key-Value Pair
+		table.insert("is",2 );
+		table.insert("a",3 );
+ 		table.insert("Test",4 );   
+		table.insert("Driver",5 );
+		System.out.println("Table Size = " + table.size());
+    // first search the key then delete it in the table
+    // see the implementation first
+		System.out.println(table.delete("is")+ " : This key is deleted from table");
+    System.out.println("Now Size of the table = " + table.size() );    
+		if(table.isEmpty())
+      System.out.println("Table is Empty");
+    else
+      System.out.println("Table is not Empty");  
+		
+	}
+}
+
+//-----------------HashEntry.java--------------
+//One Entry in the Hash Table
+class HashEntry
+{
+	String key;
+	int value;
+
+	// Reference to next node
+	HashEntry next;
+
+	// Constructor
+	public HashEntry(String key, int value)
+	{
+		this.key = key;
+		this.value = value;
+	}
+}
